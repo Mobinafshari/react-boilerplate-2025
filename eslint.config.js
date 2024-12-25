3af519902a308dc877d3da0ad87912feb41b5e2f
mobin <afshari.mobin.dev@gmail.com>
2024-12-25 18:35:17 +0330

    feat : eslint added

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,21 @@
-import js from "@eslint/js";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  js.configs.recommended,
-
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-    },
-  },
+        rules: {
+            "no-unused-vars": "error",
+            "no-undef": "error" ,
+             "prefer-const": "error",
+        }
+    }
 ];
