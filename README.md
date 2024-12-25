1b9c226f981a9d173c525a66ca1201b926818a28
mobin <afshari.mobin.dev@gmail.com>
2024-12-25 18:25:11 +0330

    feat: cypress added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # react-boilerplate-2025
+
 This is my react projects setup.
+
 <p>Install vscode biome extension to work with biome easier</p>
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 function Test() {
-  return <div>Test          </div>;
+  let name;
+  return <div>Test </div>;
 }
 
 export default Test;
