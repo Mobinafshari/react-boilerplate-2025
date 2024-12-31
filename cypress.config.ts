7619cd4a6cd9a91bb0bac745448e1dab534c727b
mobin afshari <mma137813781378@gmail.com>
2024-12-31 22:42:54 +0330

    fix: cypress updated

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,12 +3,20 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   video: false,
   chromeWebSecurity: false,
-  viewportWidth: 1280, 
+  viewportWidth: 1280,
   viewportHeight: 720,
+
   e2e: {
     baseUrl: 'http://localhost:5137',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
